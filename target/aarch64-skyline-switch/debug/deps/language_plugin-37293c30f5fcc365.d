c5f1583c4ff68f2defdf6291cc77fe873157ab8b
Deklou <azek_nui@yahoo.fr>
2025-03-15 15:36:34 +0100

    changed text

--- target/aarch64-skyline-switch/debug/deps/language_plugin-37293c30f5fcc365.d
+++ target/aarch64-skyline-switch/debug/deps/language_plugin-37293c30f5fcc365.d
@@ -1,6 +1,6 @@
-C:\Jeux\Fire_Emblem_Engage\Modding\plugins\test\target\aarch64-skyline-switch\debug\deps\liblanguage_plugin-37293c30f5fcc365.rmeta: src\lib.rs src\language.rs src\interface.rs src\voice.rs
+C:\Jeux\Fire_Emblem_Engage\Modding\plugins\Engage_Language_Plugin-master\target\aarch64-skyline-switch\debug\deps\liblanguage_plugin-37293c30f5fcc365.rmeta: src\lib.rs src\language.rs src\interface.rs src\voice.rs
 
-C:\Jeux\Fire_Emblem_Engage\Modding\plugins\test\target\aarch64-skyline-switch\debug\deps\language_plugin-37293c30f5fcc365.d: src\lib.rs src\language.rs src\interface.rs src\voice.rs
+C:\Jeux\Fire_Emblem_Engage\Modding\plugins\Engage_Language_Plugin-master\target\aarch64-skyline-switch\debug\deps\language_plugin-37293c30f5fcc365.d: src\lib.rs src\language.rs src\interface.rs src\voice.rs
 
 src\lib.rs:
 src\language.rs:
